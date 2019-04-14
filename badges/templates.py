50b1aaab37bdcb43f4d92e61111494ed39b5fa63
FearUwU <49614983+FearUwU@users.noreply.github.com>
2019-04-14 07:35:20 -0700

    Add files via upload

---
+++ badges/templates.py
@@ -0,0 +1,50 @@
+blank_template = [
+    {
+        "badge_name": "chillbar",
+        "code": "null",
+        "file_name": "https://i.imgur.com/E5o6kzq.png",
+        "is_inverted": False,
+    },
+    {
+        "badge_name": "black",
+        "code": "null",
+        "file_name": "https://i.imgur.com/UUHTHVj.png",
+        "is_inverted": False,
+    },
+    {
+        "badge_name": "blue",
+        "code": "null",
+        "file_name": "https://i.imgur.com/KlBSe9F.png",
+        "is_inverted": False,
+    },
+    {
+        "badge_name": "green",
+        "code": "null",
+        "file_name": "https://i.imgur.com/4pVwG6g.png",
+        "is_inverted": False,
+    },
+    {
+        "badge_name": "pink",
+        "code": "null",
+        "file_name": "https://i.imgur.com/k0kcbpU.png",
+        "is_inverted": False,
+    },
+    {
+        "badge_name": "red",
+        "code": "null",
+        "file_name": "https://i.imgur.com/3dgQTss.png",
+        "is_inverted": False,
+    },
+    {
+        "badge_name": "light blue",
+        "code": "null",
+        "file_name": "https://i.imgur.com/pM7ObJa.png",
+        "is_inverted": False,
+    },
+    {
+        "badge_name": "yellow",
+        "code": "null",
+        "file_name": "https://i.imgur.com/w090Nek.png",
+        "is_inverted": False,
+    },
+]
